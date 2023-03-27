40f38634dbaad6feb967ab7cb3ab6623dd05bd41
djchristiandf <djchristiandf@gmail.com>
2023-03-27 20:38:03 -0300

    front finished

--- src/api/api-request.js
+++ src/api/api-request.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const URL_API = 'http://localhost/api/prompt'
 
-export const makeRequest = asynca (message) => {
+export const makeRequest = async (message) => {
     console.log(message)
     const {data} = await axios.post(URL_API,message)
     return data;
